c359822dc2ce35020e2cd30dfa0432f3a5078bc6
ojomaze <jbobjerg@gmail.com>
2025-05-15 12:32:02 +0200

    commit before devmerge

--- src/application/model/FærdigProdukt.java
+++ src/application/model/FærdigProdukt.java
@@ -47,5 +47,28 @@ public class FærdigProdukt implements Serializable {
         return new ArrayList<>(tappetmængder);
     }
 
+    @Override
+    public String toString() {
+        return "%s %.2f%s %.2f%s".formatted(navn + ",", totalMængdeLiter, "L", endeligAlkoholdProcent, "%");
+    }
 
+    public String GUIview() {
+        var sb = new StringBuilder("Navn: " + navn);
+        sb.append("\nProdukttype: " + type.toString().toLowerCase());
+        sb.append("\nLiter tappet: " + totalMængdeLiter);
+        sb.append("\nAlkoholprocent: " + endeligAlkoholdProcent);
+        if (vandMængde > 0) {
+            double vandprocent = vandMængde / totalMængdeLiter * 100;
+            sb.append("\n%s %.1f %s (%.1f%s)".formatted("Andel vand:", vandMængde, "liter", vandprocent,"%"));
+        }
+        else {
+            sb.append("\nIntet vand tilsat");
+        }
+        sb.append("\nTappet fra " + tappetmængder.size() + " destillat");
+        if (tappetmængder.size() > 1)   {
+            sb.append("er");
+        }
+
+        return sb.toString();
+    }
 }
